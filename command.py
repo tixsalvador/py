85b4637a354e5f07285f917f0ac48de4854221c6
t|x Salvador <tix.salvador@gmail.com>
2017-09-01 13:15:04 -0400

    Update command.py

--- command.py
+++ command.py
@@ -6,3 +6,62 @@ command = subprocess.Popen('/sbin/ifconfig eth0', shell=True, stdout=subprocess.
 
 print command.stdout.read()
 output = command.communicate()[0]
+
+
+##################### 1 PIPE ################################################
+
+#!/usr/bin/env python
+
+import subprocess
+
+command = subprocess.Popen('/sbin/ifconfig eth0'.split(), stdout=subprocess.PIPE)
+pipe1 = subprocess.Popen('grep inet'.split(), stdin=command.stdout, stdout=subprocess.PIPE)
+
+command.stdout.close()
+
+print pipe1.stdout.read()
+output = pipe1.communicate()[0]
+
+############################################################################
+
+##################### 2 PIPE ################################################
+
+#!/usr/bin/env python
+
+import subprocess
+
+command = subprocess.Popen('/sbin/ifconfig eth0'.split(), stdout=subprocess.PIPE)
+pipe1 = subprocess.Popen('grep inet'.split(), stdin=command.stdout, stdout=subprocess.PIPE)
+command.stdout.close()
+
+pipe2 = subprocess.Popen(['awk', '{print $2}'], stdin=pipe1.stdout, stdout=subprocess.PIPE)
+pipe1.stdout.close()
+
+
+
+print pipe2.stdout.read()
+output = pipe2.communicate()[0]
+
+
+############################################################################
+
+##################### 3 PIPE ################################################
+
+#!/usr/bin/env python
+
+import subprocess
+
+command = subprocess.Popen('/sbin/ifconfig eth0'.split(), stdout=subprocess.PIPE)
+pipe1 = subprocess.Popen('grep inet'.split(), stdin=command.stdout, stdout=subprocess.PIPE)
+command.stdout.close()
+
+pipe2 = subprocess.Popen(['awk', '{print $2}'], stdin=pipe1.stdout, stdout=subprocess.PIPE)
+pipe1.stdout.close()
+
+pipe3 = subprocess.Popen(['awk', '-F', ':','{print $2}'], stdin=pipe2.stdout, stdout=subprocess.PIPE)
+pipe2.stdout.close()
+
+print pipe3.stdout.read()
+output = pipe3.communicate()[0]
+
+########################################################################################################
