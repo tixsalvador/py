e700eb86503316ac3a95f16e6f839cf5ecc75151
t|x Salvador <tix.salvador@gmail.com>
2017-09-17 18:20:42 -0400

    Update ping_range.py

--- ping_range.py
+++ ping_range.py
@@ -2,23 +2,25 @@
 
 import subprocess
 
-ip = subprocess.Popen('ip link', shell=True, stdout=subprocess.PIPE)
-grep1 = subprocess.Popen('grep BROADCAST,MULTICAST,UP,LOWER_UP'.split(), stdin=ip.stdout, stdout=subprocess.PIPE)
-ip.stdout.close()
+command1 = subprocess.Popen('ip addr', shell=True, stdout=subprocess.PIPE)
+grep1 = subprocess.Popen('grep BROADCAST,MULTICAST,UP,LOWER_UP'.split(), stdin=command1.stdout, stdout=subprocess.PIPE)
+command1.stdout.close()
 awk1 = subprocess.Popen(['awk','-F',':','{print $2}'], stdin=grep1.stdout, stdout=subprocess.PIPE)
 grep1.stdout.close()
-iface = awk1.communicate()[0].strip()
+ipadd = awk1.communicate()[0].strip()
 
-ipaddr = subprocess.Popen(['ip','address','show','%s' % iface], stdout=subprocess.PIPE)
-grep2 = subprocess.Popen('grep inet'.split(), stdin=ipaddr.stdout, stdout=subprocess.PIPE)
-ipaddr.stdout.close()
-awk2 = subprocess.Popen("awk '{print $2}'", shell=True, stdin=grep2.stdout, stdout=subprocess.PIPE)
+command1 = subprocess.Popen(['ip','addr','show','%s' %ipadd], stdout=subprocess.PIPE)
+grep2 = subprocess.Popen('grep -w inet'.split(), stdin=command1.stdout, stdout=subprocess.PIPE)
+command1.stdout.close()
+awk2 = subprocess.Popen(['awk','{print $2}'], stdin=grep2.stdout, stdout=subprocess.PIPE)
 grep2.stdout.close()
 awk3 = subprocess.Popen(['awk','-F','/','{print $1}'], stdin=awk2.stdout, stdout=subprocess.PIPE)
 awk2.stdout.close()
-ipaddress = awk3.communicate()[0].strip()
+ip = awk3.communicate()[0].strip()
 
-ipoct = ipaddress[:ipaddress.rfind(".")] + "."
+ipoct = ip[:ip.rfind(".")] + "."
 
 for i in range(1,10):
-  ping = subprocess.call('ping -c 1  %s%s' %(ipoct,i),shell=True)
+	print "IP %s%s" %(ipoct,i)
+	subprocess.call('ping -c 1 %s%s' %(ipoct,i), shell=True)
+	print " "
